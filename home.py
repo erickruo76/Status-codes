f9bfacfb93bffe00795b1afcc581a94d5dc7b072
Erick Ruo <erickruo72@gmail.com>
2024-05-09 16:45:35 +0300

    first

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 import requests
 
 url="https://www.tuko.co.ke/entertainment/celebrities/547640-king-kaka-wins-ksh-x-bet-staking-big-real-madrids-victory-bayern-munich/"
-web=requests.get(url)
-print(web.status_code)
+weburl=requests.get(url)
+print(weburl.status_code)
