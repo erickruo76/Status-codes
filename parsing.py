3bba9368313b5da753fb9bf4f23b93832499af64
Erick Ruo <erickruo72@gmail.com>
2024-05-10 18:11:53 +0300

    fisrt

---
+++ parsing.py
@@ -0,0 +1,10 @@
+#parsing html content using beatiful soup
+import requests
+from bs4 import BeautifulSoup
+
+url= "https://www.tuko.co.ke/"
+r=requests.get(url)
+
+
+soup  = BeautifulSoup(r.text,"lxml")
+print(soup)
