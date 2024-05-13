6fce557d89c571bad0ed3027491060eed61f9d24
Erick Ruo <erickruo72@gmail.com>
2024-05-13 13:19:11 +0300

    Find_all() function

---
+++ findAllFuncion.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url= "https://www.livescore.com/en/news/arsenal-beat-man-utd-to-put-title-race-pressure-back-on-man-city-2024051217390064438/"
+
+r=requests.get(url)
+
+desc= BeautifulSoup(r.text,"lxml")
+
+elements= desc.find_all("div",{"class":"Kh Nh"})
+
+for element in elements:
+    print(element.text.strip())
+
